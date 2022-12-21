701f3c3e60138c7d02d35d58c428ef5f3214e35f
Crypto_Vdim <vadimrace@gmail.com>
2022-12-21 18:35:53 -0500

    created random password generator, checked for user input constraints

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword(){
-    var password = "password will go here"
+    //use a 2D array that holds all of the potential character choices
+    var choicesArray = [["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+                        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z"],
+                        ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]", "^","_", "`", "{", "|", "}", "~"],
+                        ["0","1","2","3","4","5","6","7","8","9"]];
+    var password;
     var numChar;
-    var isUpperCase;
-    var isLowerCase;
-    var isNumeric;
-    var areSpecialChar;
+    var isLowerCase = false;
+    var isUpperCase = false;
+    var areSpecialChar = false;
+    var isNumeric = false;
     console.log("button pressed");
-    // PSEUDO CODING
     //1. Create prompts for password criteria
     //a. numChar>=8 && passwordChar<=128 && dataTypeOf == number
     //if input does not meet criteria, re-ask the question
     let numCharAsk = prompt("How many characters do you want your password to be?\n(please enter a number between 7 and 129)", 10);
         if(numCharAsk>=8 && numCharAsk<=128 && numCharAsk != null){
             numChar=numCharAsk;
-            console.log(numChar);
         }
     
     
@@ -24,7 +27,8 @@ function generatePassword(){
     
     
 
-    //b. charType booleans= isLowerCase, isUpperCase, isNumeric, areSpecialChar    
+    //b. charType booleans= isLowerCase, isUpperCase, isNumeric, areSpecialChar 
+    //only provide yes/no option for all charType questions use confirm() method   
     let upperCase = "Confirm OK for upper case letters";
         if (confirm(upperCase) == true) {
             upperCase = "You pressed OK!";
@@ -33,7 +37,7 @@ function generatePassword(){
             upperCase = "You canceled!";
             isUpperCase=false;
         }
-        console.log(isUpperCase);
+        console.log("isUpperCase: " + isUpperCase);
     
     let lowerCase = "Confirm OK for lower case letters";
         if (confirm(lowerCase) == true) {
@@ -43,7 +47,7 @@ function generatePassword(){
             lowerCase = "You canceled!";
             isLowerCase=false;
         }
-        console.log(isLowerCase);
+        console.log("isLowerCase: " + isLowerCase);
     
     let numeric = "Confirm OK for numbers";
         if (confirm(numeric) == true) {
@@ -53,7 +57,7 @@ function generatePassword(){
             numeric = "You canceled!";
             isNumeric=false;
         }
-        console.log(isNumeric);
+        console.log("isNumberic: " + isNumeric);
 
     let specialChar = "Confirm OK for special characters";
         if (confirm(specialChar) == true) {
@@ -63,24 +67,45 @@ function generatePassword(){
             specialChar = "You canceled!";
             areSpecialChar=false;
         }
-        console.log(areSpecialChar);
-    
-    
-    
-        
-        //document.getElementById("demo").innerHTML = text;
-
-
-
-            //only provide yes/no option for all charType questions
+        console.log("areSpecialChar: " + areSpecialChar);
+            
     //2. Generate the random password
-        //use =Math.floor(Math.random() * 10) returns a random integer between 0 and 9 (both included):
-        //use an array that holds number, letter, special char, randomize which one is used, if they prompt not to use it, dont use that element
-        //set all alphabet letters to each element of an array, use Math.random to that size to go through all the letters
-        //set isLowerCase to a 2 element array, math.random 0 or 1
-    
+        //use =Math.floor(Math.random() * 10)
+        //check outer portion of array, if one of the constraints is false, re-generate a random
+        //number until constraints are satisfied.
+    function randomConstraintChecker(){
+        var checkedValue=-1;
+        var valueGTG=false;
+        do{
+            checkedValue=Math.floor(Math.random()*(choicesArray.length));
+            if(checkedValue==0 && isLowerCase){
+                valueGTG=true;
+            }
+            if(checkedValue==1 && isUpperCase){
+                valueGTG=true;
+            }
+            if(checkedValue==2 && areSpecialChar){
+                valueGTG=true;
+            }
+            if(checkedValue==3 && isNumeric){
+                valueGTG=true;
+            }
+        }while(!valueGTG);
+        console.log("checkedValue: " + checkedValue);
+        return checkedValue;
+    }
+    //generate the password based on number of characters and constraint checker function.
+    for(var i =0; i<numChar; i++){
+        var outerElement=randomConstraintChecker();
+        var innerElement = Math.floor(Math.random()*(choicesArray[outerElement].length));
 
-
+        if(password==undefined){
+            password=choicesArray[outerElement][innerElement];
+        }else{
+            password=password.concat(choicesArray[outerElement][innerElement]);
+        }
+        console.log(password);
+    }
     return password;
 }
 
