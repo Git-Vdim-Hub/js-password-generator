6d5a7b5d26c9833c167974429b615075151e0e0d
Crypto_Vdim <vadimrace@gmail.com>
2022-12-20 23:01:51 -0500

    added all window prompts

--- script.js
+++ script.js
@@ -2,18 +2,76 @@
 var generateBtn = document.querySelector("#generate");
 function generatePassword(){
     var password = "password will go here"
+    var numChar;
+    var isUpperCase;
+    var isLowerCase;
+    var isNumeric;
+    var areSpecialChar;
     console.log("button pressed");
     // PSEUDO CODING
     //1. Create prompts for password criteria
-    let person = prompt("Please enter your name", "Harry Potter");
+    //a. numChar>=8 && passwordChar<=128 && dataTypeOf == number
+    //if input does not meet criteria, re-ask the question
+    let numCharAsk = prompt("How many characters do you want your password to be?\n(please enter a number between 7 and 129)", 10);
+        if(numCharAsk>=8 && numCharAsk<=128 && numCharAsk != null){
+            numChar=numCharAsk;
+            console.log(numChar);
+        }
+    
+    
+        
+    //atleast 1 character type needs to be selected, restart all questions if no selected on all choices, restart char prompts 
+    
+    
 
-    if (person != null) {
-         console.log(person);
-    }
-        //a. passwordChar>=8 && passwordChar<=128 && dataTypeOf == number
-            //if input does not meet criteria, re-ask the question
-        //b. charType booleans= isLowerCase, isUpperCase, isNumeric, areSpecialChar
-            //atleast 1 character type needs to be selected, restart all questions if no selected on all choices, restart char prompts
+    //b. charType booleans= isLowerCase, isUpperCase, isNumeric, areSpecialChar    
+    let upperCase = "Confirm OK for upper case letters";
+        if (confirm(upperCase) == true) {
+            upperCase = "You pressed OK!";
+            isUpperCase=true;
+        } else {
+            upperCase = "You canceled!";
+            isUpperCase=false;
+        }
+        console.log(isUpperCase);
+    
+    let lowerCase = "Confirm OK for lower case letters";
+        if (confirm(lowerCase) == true) {
+            lowerCase = "You pressed OK!";
+            isLowerCase=true;
+        } else {
+            lowerCase = "You canceled!";
+            isLowerCase=false;
+        }
+        console.log(isLowerCase);
+    
+    let numeric = "Confirm OK for numbers";
+        if (confirm(numeric) == true) {
+            numeric = "You pressed OK!";
+            isNumeric=true;
+        } else {
+            numeric = "You canceled!";
+            isNumeric=false;
+        }
+        console.log(isNumeric);
+
+    let specialChar = "Confirm OK for special characters";
+        if (confirm(specialChar) == true) {
+            specialChar = "You pressed OK!";
+            areSpecialChar=true;
+        } else {
+            specialChar = "You canceled!";
+            areSpecialChar=false;
+        }
+        console.log(areSpecialChar);
+    
+    
+    
+        
+        //document.getElementById("demo").innerHTML = text;
+
+
+
             //only provide yes/no option for all charType questions
     //2. Generate the random password
         //use =Math.floor(Math.random() * 10) returns a random integer between 0 and 9 (both included):
